562999af33d8d5e49668ecc9aee07b8f13462186
rubayet-hasan-yasin <rubayethasanyasin@gmail.com>
2025-07-29 00:37:42 +0600

    feat: extend CourseData interface with Section and SectionValue

--- src/interface/courseData.ts
+++ src/interface/courseData.ts
@@ -18,6 +18,50 @@ export interface cta_text {
   value: string;
 }
 
+interface SectionValue {
+    // Common fields
+    id?: string;
+
+    // Instructor values
+    description?: string;
+    has_instructor_page?: boolean;
+    image?: string;
+    name?: string;
+    short_description?: string;
+    slug?: string;
+
+    // Feature values
+    icon?: string;
+    subtitle?: string;
+    title?: string;
+
+    // Engagement values
+    background?: {
+        image: string;
+        primary_color: string;
+        secondary_color: string;
+    };
+    cta?: {
+        clicked_url: string;
+        color: string;
+        text: string;
+    };
+    description_color?: string;
+    thumbnail?: string;
+    title_color?: string;
+    top_left_icon_img?: string;
+}
+
+
+interface Section {
+    type: string;
+    name: string;
+    description: string;
+    bg_color: string;
+    order_idx: number;
+    values: SectionValue[];
+}
+
 export interface CourseData {
   code: number;
   data: {
@@ -28,6 +72,7 @@ export interface CourseData {
     description: string;
     platform: string;
     type: string;
+    sections: Section[];
     modality: string;
     media: MediaItem[];
     checklist: ChecklistItem[];
